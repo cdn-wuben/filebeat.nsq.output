40ced5e31d758d8cc5f466a77063d242549d1a7b
cdn-wuben <40381200+cdn-wuben@users.noreply.github.com>
2019-03-13 16:22:24 +0800

    Update client.go

--- nsqout/client.go
+++ nsqout/client.go
@@ -110,9 +110,11 @@ func (c *client) Publish(batch publisher.Batch) error {
 func (c *client) buildNsqMessages(events []publisher.Event) ([][]byte, error) {
 	length := len(events)
 	msgs := make([][]byte, length)
+	var total sync.Map
 	var count int
-
+	maplog := make(map[string]interface{})
 	var err error
+	
 	for idx := 0; idx < length; idx++ {
 		event := events[idx].Content
 		serializedEvent, nerr := c.codec.Encode(c.index, &event)
@@ -123,10 +125,56 @@ func (c *client) buildNsqMessages(events []publisher.Event) ([][]byte, error) {
 		} else {
 			//should copy, see https://blog.golang.org/go-slices-usage-and-internals
 			tmp := string(serializedEvent)
+			data := strings.Split(tmp, "\t")
+			time1 := data[0]
+			loc, _ := time.LoadLocation("Local")
+			times, _ := time.ParseInLocation("02/Jan/2006:15:04:05 +0800", time1, loc)
+			timestamp := times.Unix()
+			ts = timestamp - timestamp%300
+			domain: = data[9]
+			command: = data[5]
+			bytes_received: = strconv.ParseInt(data[6], 10, 64)
+			bytes_sent: = strconv.ParseInt(data[7], 10, 64)
+			key:=domain+" "+ts+" "+command
+			vv, _ := total.Load(key)
+            if vv != nil {
+                vc, _ := vv.(string)
+				value := strings.Split(filemap[key], "")
+				rflow := strconv.Atoi(value[0])
+				sflow := strconv.Atoi(value[1])
+				rf := strconv.Itoa(rflow + body_received)
+				sf := strconv.Itoa(sflow + bytes_sent)
+                total.Delete(key)
+                total.Store(key,rf + " " + sf)
+            } else {
+                total.Store(key,strconv.Itoa(bytes_received) + " " + strconv.Itoa(bytes_sent)
+            }
 			//logp.Info("main:nsq] BuildMessage: %v", string(serializedEvent))
-			msgs[count] = []byte(tmp)
-			count++
 		}
 	}
+	total.Range(func(k, v interface{}) bool {
+		sk := strings.Split(k.(string), " ")
+        timestamp, _ := strconv.ParseInt(sk[1], 10, 64)
+        maplog["domain"] = sk[0]
+        maplog["timestamp"] = timestamp
+        maplog["command"] = sk[2]
+       	vs = v.(string)
+		value := strings.Split(vs, "")
+		rflow := strconv.Atoi(value[0])
+		sflow := strconv.Atoi(value[1])
+		maplog["bytes_received"]=rflow
+		maplog["bytes_send"]=sflow
+        total.Delete(k)
+        jsonstr, err := json.Marshal(maplog)
+        if err != nil {
+             panic(err)
+        }
+        if jsonstr == nil {
+			logmsg:=string(jsonstr)
+			msgs[count] = []byte(logmsg)	        
+			count++
+		}
+
 	return msgs[:count], err
 }
+
